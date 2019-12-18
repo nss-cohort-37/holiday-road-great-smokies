713c8a31fc1b4d8e1cb66ab88c3591b23f5fab01
jansenv <jansenv44@gmail.com>
2019-12-18 17:27:53 -0600

    experimenting with weather component

--- scripts/weather/WeatherCard.js
+++ scripts/weather/WeatherCard.js
@@ -5,9 +5,8 @@ import { getWeather, useWeather } from "./WeatherProvider.js"
 
 export const weatherComponent = () => {
 
-
-const eventHub = document.querySelector(".container")
-const contentTarget = document.querySelector(".weathertest")
+  const eventHub = document.querySelector(".container")
+  const contentTarget = document.querySelector(".weathertest")
 
 
   eventHub.addEventListener("weatherParkSelected", event => {
@@ -17,9 +16,20 @@ const contentTarget = document.querySelector(".weathertest")
     const [longprefix, long] = longitude.split("long:")
     const apiKey = "2aa5986f620fa1dd066ffd051d2a7173"
     getWeather(lat, long, apiKey).then((weather) => weather = useWeather())
+    render()
   })
-  
-
 
   
+  const render = (weatherData) => {
+    contentTarget.innerHTML = `
+      <p>Description: ${"foo"}</p>
+      <p>Temperature: ${"foo"}</p>
+      <p>Humidity: ${"foo"}</p>
+      <p>Wind: ${"foo"}</p>
+    `
+  }
+
+
+  render()
+
 }
